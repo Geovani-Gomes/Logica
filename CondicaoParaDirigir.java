5a646ac14d9653d759dd9eb5bed78bdab0990d76
Geovani-Gomes <geovani.gomes012@gmailcom>
2024-02-08 17:37:37 -0300

    logica

---
+++ CondicaoParaDirigir.java
@@ -0,0 +1,14 @@
+public class CondicaoParaDirigir{
+    public static void main(String[] args) {
+        String name = "Marcos";
+        int age = 19;
+
+        if(age >= 18){
+            System.out.println("O " + name + " pode dirigir");
+        }
+        else{
+            System.out.println("O " + name + " não pode dirigir");
+        }
+        
+    }
+}       
